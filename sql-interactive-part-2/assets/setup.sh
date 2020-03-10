0734f87c91c4902c7149474992f0f149ab3bd835
Dimitris Koutsoukos <dkoutsou@student.ethz.ch>
2020-03-10 11:19:07 +0100

    Second interactive session setup

---
+++ sql-interactive-part-2/assets/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get install wget ca-certificates
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+apt-get update
+apt-get install -y postgresql postgresql-contrib
+pg_ctlcluster 12 main start
+su - postgres
+psql
